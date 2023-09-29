5c1d259c854da551fc26a4040cef0e0a3107e23e
Shinn Thant Swam Ye <1shinnthantswamye@gmail.com>
2023-09-30 02:50:42 +0630

    Add component for individual book

---
+++ book-store/src/components/book.js
@@ -0,0 +1,36 @@
+import PropTypes from 'prop-types';
+
+const Book = ({ bookInfo }) => (
+  <div>
+    <div className="description">
+      <p className="category">{bookInfo.category}</p>
+      <h3 className="title">{bookInfo.title}</h3>
+      <small className="author">{bookInfo.author}</small>
+      <div className="btnContainer">
+        <button type="button" className="remove">Remove</button>
+        <button type="button" className="edit">Edit</button>
+      </div>
+    </div>
+    <div className="borderV" />
+    <div className="chapter">
+      <p>
+        <span>Current chapter</span>
+        <br />
+        {bookInfo.chapter}
+      </p>
+      <button type="button" className="update">Update Progress</button>
+    </div>
+  </div>
+);
+
+Book.propTypes = {
+  bookInfo: PropTypes.shape({
+    category: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    author: PropTypes.string.isRequired,
+    progress: PropTypes.string.isRequired,
+    chapter: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+export default Book;
