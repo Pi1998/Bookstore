3828cdcce1d1753b68c83aea89dd9c23beda6003
Shinn Thant Swam Ye <1shinnthantswamye@gmail.com>
2023-10-05 13:33:55 +0630

    Delete array and use booksAry

--- book-store/src/components/booksList.js
+++ book-store/src/components/booksList.js
@@ -1,30 +1,8 @@
-import { useState } from 'react';
+import { useSelector } from 'react-redux';
 import Book from './book';
 
 const BooksList = () => {
-  const [booksInfo] = useState([
-    {
-      id: 1,
-      category: 'Science Fiction',
-      title: 'Interstaller',
-      author: 'Frank',
-      chapter: 'Chapter 4',
-    },
-    {
-      id: 2,
-      category: 'Drama',
-      title: 'Kiminonawa',
-      author: 'Herbert',
-      chapter: 'Chapter 2',
-    },
-    {
-      id: 3,
-      category: 'Comic',
-      title: 'Ironman',
-      author: 'Stan Lee',
-      chapter: 'Chapter 7',
-    },
-  ]);
+  const booksInfo = useSelector((state) => state.books.booksAry);
 
   return (
     <div className="booksList">
