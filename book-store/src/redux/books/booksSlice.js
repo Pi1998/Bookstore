6bde6257b50237943f28910549ab61c9e9cb5869
Shinn Thant Swam Ye <1shinnthantswamye@gmail.com>
2023-10-06 16:52:46 +0630

    Add bookstore api, unique id, use axios and Thunk

--- book-store/src/redux/books/booksSlice.js
+++ book-store/src/redux/books/booksSlice.js
@@ -1,38 +1,68 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
 
 const initialState = {
-  booksAry: [
-    {
-      item_id: '1',
-      title: 'The Great Gatsby',
-      author: 'John Smith',
-      category: 'Fiction',
-    },
-    {
-      item_id: '2',
-      title: 'Anna Karenina',
-      author: 'Leo Tolstoy',
-      category: 'Fiction',
-    },
-    {
-      item_id: '3',
-      title: 'The Selfish Gene',
-      author: 'Richard Dawkins',
-      category: 'Nonfiction',
-    },
-  ],
+  booksAry: [],
+  isLoading: false,
+  error: null,
 };
+
+const baseURL = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi';
+axios.defaults.baseURL = baseURL;
+
+const YOUR_APP_ID = 'eEQS3mmCqcp18Mjzt03t';
+
+export const fetchBooks = createAsyncThunk('books/fetchBooks', async () => {
+  try {
+    const response = await axios.get(`/apps/${YOUR_APP_ID}/books`);
+    return response.data;
+  } catch (error) {
+    throw new Error('Error fetching books');
+  }
+});
+
+export const addBookAsync = createAsyncThunk('books/addBook', async (book) => {
+  try {
+    await axios.post(`/apps/${YOUR_APP_ID}/books`, book);
+    return book;
+  } catch (error) {
+    throw new Error('Error adding book');
+  }
+});
+
+export const removeBookAsync = createAsyncThunk('books/removeBook', async (itemId) => {
+  try {
+    await axios.delete(`/apps/${YOUR_APP_ID}/books/${itemId}`);
+    return itemId;
+  } catch (error) {
+    throw new Error('Error removing book');
+  }
+});
 
 const booksSlice = createSlice({
   name: 'books',
   initialState,
   reducers: {
     addBook: (state, action) => {
-      state.booksAry = [...state.booksAry, action.payload];
+      state.booksAry.push(action.payload);
     },
     removeBook: (state, action) => {
       state.booksAry = state.booksAry.filter((book) => book.item_id !== action.payload);
     },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchBooks.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(fetchBooks.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.booksAry = action.payload;
+      })
+      .addCase(fetchBooks.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.error.message;
+      });
   },
 });
 
