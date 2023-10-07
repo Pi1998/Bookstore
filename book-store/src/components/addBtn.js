ad5d1c40e85f93017e1454850b53895630209b50
Shinn Thant Swam Ye <1shinnthantswamye@gmail.com>
2023-10-07 17:22:00 +0630

    Import css

--- book-store/src/components/addBtn.js
+++ book-store/src/components/addBtn.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import '../styles/addBook.css';
 
 const AddButton = ({ onClick, children }) => (
   <button type="submit" onClick={onClick} className="btnAdd">
